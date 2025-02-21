c6b8994d4f88256e11f1eb5673115feb193c2172
Pma10 <leejhan0719@naver.com>
2025-02-22 00:04:36 +0900

    :rocket: 안정성 업데이트

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv, find_dotenv
 import asyncio
 
-
 load_dotenv(find_dotenv())
 
 MONGO_MAIN = os.getenv("MONGO_MAIN")
 MONGO_BACKUP = os.getenv("MONGO_BACKUP")
 BACKUP_DBS = os.getenv("BACKUP_DBS").split(',')
+BATCH_SIZE = os.getenv("BATCH_SIZE")
 
 client_main = AsyncIOMotorClient(MONGO_MAIN)
-client_backup =  AsyncIOMotorClient(MONGO_BACKUP)
-
+client_backup = AsyncIOMotorClient(MONGO_BACKUP)
 
 async def backup_data():
     for db in BACKUP_DBS:
         colls = await client_main[db].list_collection_names()
         for coll in colls:
-            data = await client_main[db][coll].find().to_list(length=None)
-            await client_backup[db][coll].insert_many(data)
-            print(f"Backup {db}/{coll} done")
+            source_collection = client_main[db][coll]
+            target_collection = client_backup[db][coll]
 
+            async for doc in source_collection.find():
+                doc_id = doc["_id"]
+                await target_collection.replace_one({"_id": doc_id}, doc, upsert=True)
+
+            print(f"Backup {db}/{coll} done")
 
 if __name__ == '__main__':
     asyncio.run(backup_data())
