7652673acdcfb351d998b231a300b776eecb03a1
Pma10 <leejhan0719@naver.com>
2025-02-22 00:22:08 +0900

    :rocket: 안정성 업데이트

--- main.py
+++ main.py
@@ -3,28 +3,34 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv, find_dotenv
 import asyncio
 
+
 load_dotenv(find_dotenv())
 
 MONGO_MAIN = os.getenv("MONGO_MAIN")
 MONGO_BACKUP = os.getenv("MONGO_BACKUP")
 BACKUP_DBS = os.getenv("BACKUP_DBS").split(',')
-BATCH_SIZE = os.getenv("BATCH_SIZE")
 
 client_main = AsyncIOMotorClient(MONGO_MAIN)
 client_backup = AsyncIOMotorClient(MONGO_BACKUP)
 
+
 async def backup_data():
-    for db in BACKUP_DBS:
-        colls = await client_main[db].list_collection_names()
-        for coll in colls:
-            source_collection = client_main[db][coll]
-            target_collection = client_backup[db][coll]
+    for db_name in BACKUP_DBS:
+        db_main = client_main[db_name]
+        db_backup = client_backup[db_name]
 
-            async for doc in source_collection.find().batch_size(BATCH_SIZE):
-                doc_id = doc["_id"]
-                await target_collection.replace_one({"_id": doc_id}, doc, upsert=True)
+        collections = await db_main.list_collection_names()
 
-            print(f"Backup {db}/{coll} done")
+        for coll_name in collections:
+            collection_main = db_main[coll_name]
+            collection_backup = db_backup[coll_name]
+
+            async for document in collection_main.find():
+                doc_id = document["_id"]
+                await collection_backup.replace_one({"_id": doc_id}, document, upsert=True)
+
+            print(f"Backup : {db_name}/{coll_name} Done")
+
 
 if __name__ == '__main__':
     asyncio.run(backup_data())
